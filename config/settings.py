b4f3ed11859e58dcaef61e96961b0f85688a022c
n-thanh <thban2803@gmail.com>
2022-09-02 15:12:18 +0900

    Edit connecting DB

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'db',
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': 5432,
     }
 }
