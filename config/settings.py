1f28e7900bd1b80eab10766d3cd62f39439233d6
n-thanh <thban2803@gmail.com>
2022-09-02 17:51:24 +0900

    Deploy to heroku

--- config/settings.py
+++ config/settings.py
@@ -17,9 +17,11 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 import mimetypes
+import django_heroku
 
 load_dotenv('.env')
 mimetypes.add_type("text/css", ".css", True)
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,6 +148,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
