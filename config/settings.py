864931bd2b0ced22f12667abe69f37117b2e3a36
n-thanh <thban2803@gmail.com>
2022-09-02 16:50:58 +0900

    Add mimetypes to load local assets

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,10 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+import mimetypes
+
 load_dotenv('.env')
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
